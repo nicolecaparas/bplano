6a0a71dc1f0a3495345e17e005da71dcaa3ae90e
Nicky Caparas <nicky@nickycaparas.com>
2022-01-18 18:09:40 +1300

    Create Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Footer () {
+  return (
+    <div className="Foot">
+        <footer className="p-10 footer bg-yellow-400 text-white font-sans footer-center">
+  <div className="grid grid-flow-col gap-4">
+    <Link to="./ourstory" className="link link-hover">Our Story</Link> 
+    <Link to="./contact" className="link link-hover">Contact</Link> 
+  </div> 
+  <div>
+    <div className="grid grid-flow-col gap-4">
+    <i class="fab fa-instagram fa-3x text-white"></i> 
+    <i class="fab fa-facebook-square fa-3x text-white"></i>
+    <i class="fab fa-youtube fa-3x text-white"></i>
+    </div>
+  </div> 
+  <div>
+    <p>Copyright © 2022 - All right reserved by BPlano</p>
+  </div>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
