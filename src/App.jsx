6f3d3e8747044e60ac355bceade454aa8ca98164
Nicky Caparas <nicky@nickycaparas.com>
2022-02-16 22:38:15 +1300

    Create Planner Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './Header'
 import Landing from './Landing'
 import BoxPlanner from './BoxPlanner'
+import Planner from './Planner'
 import Contact from './Contact'
 import OurStory from './OurStory'
 import Footer from './Footer'
@@ -17,6 +18,7 @@ function App() {
         <Route index element={<Landing />} />
 
         <Route path='/boxplanner' element={<BoxPlanner />} />
+        <Route path='/planner' element={<Planner />} />
         <Route path='/ourstory' element={<OurStory />} />
         <Route path='/contact' element={<Contact />} />
       </Route>
